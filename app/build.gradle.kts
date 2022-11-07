583811be25299b274a9ad09fc604d0f6ab460dfe
Yongjin Jung <supertom0315@gmail.com>
2022-11-07 21:16:45 +0900

    Add something

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,9 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.5.1")
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
+    implementation("androidx.activity:activity-ktx:1.6.1")
+    implementation("androidx.fragment:fragment-ktx:1.5.4")
     implementation("com.google.android.material:material:1.7.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
