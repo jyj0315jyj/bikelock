5505502ad88751dbefc018063da45f167d1db681
Yongjin Jung <supertom0315@gmail.com>
2022-11-10 11:11:25 +0900

    Add some arduino codes again

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,9 +1,20 @@
+#include <Arduino_LSM6DS3.h>
+
 void setup() {
   // put your setup code here, to run once:
-
+  IMU.begin();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if(IMU.gyroscopeAvailable()) {
+    float x, y, z;
+    Serial.println("---------------")
+    Serial.print("X: ");
+    Serial.println(x);
+    Serial.print("Y: ");
+    Serial.println(y);
+    Serial.print("Z: ");
+    Serial.println(z);
+  }
 }
